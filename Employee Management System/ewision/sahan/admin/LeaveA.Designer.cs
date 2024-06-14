893d6211a110ac1220d5f0619b0c2c60da69413e
Prabugs <chathuraprabash4218@gmail.com>
2024-06-15 03:04:15 +0530

    Leave Table set

--- Employee Management System/ewision/sahan/admin/LeaveA.Designer.cs
+++ Employee Management System/ewision/sahan/admin/LeaveA.Designer.cs
@@ -28,32 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dateTimePicker1 = new DateTimePicker();
-            textBox1 = new TextBox();
             button1 = new Button();
             panel1 = new Panel();
+            leaaveTable = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewButtonColumn();
+            Column5 = new DataGridViewButtonColumn();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)leaaveTable).BeginInit();
             SuspendLayout();
-            // 
-            // dateTimePicker1
-            // 
-            dateTimePicker1.Location = new Point(34, 647);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(208, 23);
-            dateTimePicker1.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            textBox1.BackColor = SystemColors.MenuText;
-            textBox1.BorderStyle = BorderStyle.None;
-            textBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.ForeColor = SystemColors.GradientInactiveCaption;
-            textBox1.Location = new Point(277, 637);
-            textBox1.MinimumSize = new Size(447, 43);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(460, 43);
-            textBox1.TabIndex = 1;
-            textBox1.Text = " Reason";
-            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // button1
             // 
@@ -61,22 +46,64 @@
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button1.ForeColor = Color.White;
-            button1.Location = new Point(774, 637);
+            button1.Location = new Point(410, 649);
             button1.Name = "button1";
             button1.Size = new Size(150, 43);
             button1.TabIndex = 25;
-            button1.Text = "Submit";
+            button1.Text = "Refresh";
             button1.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
             panel1.BackColor = Color.LightGray;
-            panel1.Location = new Point(34, 26);
+            panel1.Controls.Add(leaaveTable);
+            panel1.Location = new Point(12, 12);
             panel1.Name = "panel1";
-            panel1.Size = new Size(902, 589);
+            panel1.Size = new Size(947, 603);
             panel1.TabIndex = 26;
             // 
-            // Leave
+            // leaaveTable
+            // 
+            leaaveTable.AllowUserToAddRows = false;
+            leaaveTable.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            leaaveTable.BackgroundColor = SystemColors.ButtonHighlight;
+            leaaveTable.BorderStyle = BorderStyle.Fixed3D;
+            leaaveTable.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            leaaveTable.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
+            leaaveTable.Cursor = Cursors.Hand;
+            leaaveTable.GridColor = SystemColors.Window;
+            leaaveTable.Location = new Point(16, 243);
+            leaaveTable.Name = "leaaveTable";
+            leaaveTable.Size = new Size(917, 343);
+            leaaveTable.TabIndex = 1;
+            leaaveTable.CellClick += leaaveTable_CellClick;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Employee Name";
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Mobile";
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Leave Data";
+            Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Accept";
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "Decline";
+            Column5.Name = "Column5";
+            // 
+            // LeaveA
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
@@ -84,20 +111,22 @@
             ClientSize = new Size(971, 725);
             Controls.Add(panel1);
             Controls.Add(button1);
-            Controls.Add(textBox1);
-            Controls.Add(dateTimePicker1);
-            Name = "Leave";
+            Name = "LeaveA";
             Text = "Leave";
             Load += Leave_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)leaaveTable).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private DateTimePicker dateTimePicker1;
-        private TextBox textBox1;
         private Button button1;
         private Panel panel1;
+        private DataGridView leaaveTable;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewButtonColumn Column4;
+        private DataGridViewButtonColumn Column5;
     }
 }
--- Employee Management System/ewision/sahan/admin/LeaveA.cs
+++ Employee Management System/ewision/sahan/admin/LeaveA.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Employee_Management_System.ewision.sahan.model;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,43 @@ namespace Employee_Management_System.admin_dashboard_pages
         public LeaveA()
         {
             InitializeComponent();
+            DisplayLeaveData(leaaveTable);
+        }
+
+        public static void DisplayLeaveData(DataGridView dvg)
+        {
+
+            string query = "SELECT * FROM `leaves` INNER JOIN `user` ON `user`.`uid`=`leaves`.`user_uid`";
+
+            MySqlDataReader resultSet = MySQL.Execute(query);
+
+            if (resultSet.HasRows)
+            {
+                dvg.Rows.Clear();
+
+                while (resultSet.Read())
+                {
+
+                    DataGridViewRow newRow = new DataGridViewRow();
+
+                    newRow.CreateCells(dvg);
+                    newRow.Cells[0].Value = resultSet["fname"] + " " + resultSet["lname"];
+                    newRow.Cells[1].Value = resultSet["mobile"];
+                    newRow.Cells[2].Value = resultSet["reason"];
+                    newRow.Cells[3].Value = "Accept";
+                    newRow.Cells[4].Value = "Reject";
+
+
+                    dvg.Rows.Add(newRow);
+
+                }
+
+
+
+            }
+
+
+
         }
 
         private void Leave_Load(object sender, EventArgs e)
@@ -25,6 +64,21 @@ namespace Employee_Management_System.admin_dashboard_pages
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void leaaveTable_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 3) {
+                return;
+            }
+            if (e.ColumnIndex == 4) {
+
+
+                string query = "UPDATE `leaves` SET     ";
+
+                MySqlDataReader resultSet = MySQL.Execute(query);
+
+            }
         }
     }
 }
